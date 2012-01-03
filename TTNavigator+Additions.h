6eba6458633a7a936b6fa72d8c483835f2652ee2
Colin Young <me@colinyoung.com>
2012-01-03 15:10:12 -0600

    Commented the methods.

--- TTNavigator+Additions.h
+++ TTNavigator+Additions.h
@@ -4,15 +4,31 @@
 #define kHorizontalSlideAnimationDuration 0.35f
 #define kModalTransitionDuration 0.25f
 
+/* Useful when determining what class of View controller a URL points to:
+  for example, tt://home -> HomeController
+*/ 
 -(Class)classOfViewControllerForURL:(NSURL *)URL;
 
 -(void)popToRootViewControllerAnimated:(BOOL)animated;
 -(void)popToRootViewControllerAnimated:(BOOL)animated thenOpenURLAction:(TTURLAction*)action;
+
+/* Pops only ONE view controller back. Essentially programmatically presses the back button,
+  however, includes some niceties such as dismissing modals if the topViewController is modal.
+  
+  NOTE, that for that to work, you need to implement a 
+  -(BOOL)presentsAsModal method on that modal view controller returning YES.
+*/
 -(void)popViewControllerAnimated:(BOOL)animated;
--(void)popViewControllerAnimated:(BOOL)animated afterwards:(void (^)(void))afterwards;
+-(void)popViewControllerAnimated:(BOOL)animated afterwards:(void (^)(void))afterwards; // afterwards is a block (^{})
+
+/* The root view controller is considered the "base" view controller.
+  This method does _not_ remove the root view controller. */
 -(void)popToRootViewControllerThenOpenURLs:(NSArray*)URLs;
+
+/* This method completely clears your navigation stack. Useful at login, logout, etc. */
 -(void)clearRootViewControllerThenOpenURLAction:(TTURLAction*)action;
 -(void)clearRootViewControllerThenOpenURLAction:(TTURLAction*)action afterDelay:(CGFloat)delay;
+
 -(void)dismissModalViewController:(UIViewController *)viewController thenOpenURLAction:(TTURLAction *)URLAction animated:(BOOL)animated;
 
 // Private
